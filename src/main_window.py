1981c600e6f376aa35ca979a6003e95c721e9968
Sourabh S Nath <sourabhsnath@gmail.com>
2022-02-24 16:21:12 +0530

    Add features for sending and reciving data; Add codes for interacting with database and showing data in the table

--- src/main_window.py
+++ src/main_window.py
@@ -1,25 +1,45 @@
 from PyQt6 import QtWidgets
 
+from device_database import DeviceDatabase
 from gui import main_communication_window
-# import serial_communication
+from save_window import SaveDialogWindow
 from serial_communication import SerialCommunication
 
 
 class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow):
-    previous_read_data = ""
+    # List of previously read messages. Class variable for now.
+    previous_read_data_list = []
 
     def __init__(self):
         super().__init__()
         self.serial_communication = SerialCommunication()
         self.setupUi(self)
+        self.db = DeviceDatabase()
         self.serial_devices = []
-        self.current_device = {}
+        self.current_device = {}  # TO keep track of the current device. Unused for now.
         self.is_device_found = False
+        self.setup_table()
         self.is_device_connected = False
         self.setup_baud_rate()
+        self.update_table()
+        self.save_dialog = SaveDialogWindow()
         self.search_device_button.clicked.connect(self.search_devices)
         self.connect_button.clicked.connect(self.serial_connection)
         self.send_message_button.clicked.connect(self.write_to_device)
+        self.save_to_database_button.clicked.connect(self.save_data)
+
+    def setup_table(self):
+        column_count = 7
+        self.saved_table.setColumnCount(column_count)
+        self.saved_table.setHorizontalHeaderLabels(
+            ["Device Name", "Product Name", "Serial Number", "Baud Rate", "Priority Bits", "Data Bits", "Port Name"])
+        header = self.saved_table.horizontalHeader()
+        header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeMode.ResizeToContents)
+
+        self.saved_table.setStyleSheet("QTableWidget::item {padding: 8px}")
+        self.saved_table.verticalHeader().setVisible(False)
 
     # Set Baud rates programmatically since the numbers aren't known.
     def setup_baud_rate(self):
@@ -31,6 +51,7 @@ class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow)
     # Search all serial communication devices connected to the system
     def search_devices(self):
         self.serial_devices = self.serial_communication.get_all_devices()
+        self.statusbar.showMessage("Searching")
         for i in self.serial_devices:
             for key in i:
                 if key == "Device":
@@ -39,11 +60,12 @@ class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow)
                     self.port_input.setText(i[key])
                 if key == "Serial Number":
                     self.serial_no_input.setText(i[key])
-                # if key == "Port":
-                #     serial_communication.port_location = (i[key])
 
         if len(self.serial_devices) != 0:
             self.is_device_found = True
+            self.statusbar.showMessage("Device Found", msecs=1500)
+        else:
+            self.statusbar.showMessage("No devices found.")
 
     # Connect to the device currently seen in the combobox, currently connects to the first serial_device list item
     # in the combo box.
@@ -59,8 +81,11 @@ class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow)
                         self.serial_communication.connection(current_device["Port"],
                                                              self.baud_rate_combo_box.currentText())
                         self.is_device_connected = self.serial_communication.get_connection_status()
+                        print("IS Device Connected", self.is_device_connected)
+                        self.statusbar.showMessage("Connecting to device", msecs=500)
             else:
                 self.connect_button.setText("Connect")
+                self.statusbar.showMessage("Disconnect", msecs=1500)
         else:
             call_error_msg_box("Please select a device.")
 
@@ -69,17 +94,59 @@ class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow)
             message = self.send_message_input.toPlainText()
             print("Send to device", message)
             self.serial_communication.write_data(message)
+            self.statusbar.showMessage("Sending", msecs=400)
             # This needs to be automatic by using threading or some observer pattern, here it just checks the
             # previous value and makes changes if only the new value is different.
-            if self.serial_communication.received_data != MainWindow.previous_read_data:
-                MainWindow.previous_read_data = self.serial_communication.received_data
-                self.update_received_message(self.serial_communication.received_data)
-            self.send_message_input.clear()
+            if self.serial_communication.received_data_list != MainWindow.previous_read_data_list:
+                MainWindow.previous_read_data_list = self.serial_communication.received_data_list[:]
+                print("MainWindow previous_read_data_list", MainWindow.previous_read_data_list)
+                for msg in MainWindow.previous_read_data_list:
+                    self.update_received_message(msg)
+                self.send_message_input.clear()
+            else:
+                call_error_msg_box("Please enter a message.")
         else:
-            call_error_msg_box("Please check if the device is connected.")
+            call_error_msg_box("Please check whether the device is connected.")
 
     def update_received_message(self, msg):
         self.recieved_message_text_output.append(msg.decode("utf-8"))
+
+    def save_data(self):
+        if self.serial_devices:
+            product_name = self.device_combox_box.currentText()
+            port_name = self.port_input.text()
+            serial_number = self.serial_no_input.text()
+            baud_rate = self.baud_rate_combo_box.currentText()
+            parity = self.parity_combobox.currentText()
+            data_bits = self.data_bit_combobox.currentText()
+            self.save_dialog.open_save_data_window(db=self.db, product_name=product_name, port_name=port_name,
+                                                   serial_number=serial_number,
+                                                   baud_rate=baud_rate, parity=parity, data_bits=data_bits)
+
+            self.update_table()
+        else:
+            call_error_msg_box("Please check the inputs.")
+
+    def update_table(self):
+        results = self.db.get_table_data()
+        print("Table Data:", results)
+        self.saved_table.setRowCount(len(results))
+        for row_count, row_data in enumerate(results):
+            for col_count, data in enumerate(row_data):
+
+                # Skipping index from the table.
+                if col_count != 0:
+                    if col_count == 5:
+                        if data == '0':
+                            parity = "Odd"
+                        elif data == 'E':
+                            parity = "Even"
+                        else:
+                            parity = "No Parity"
+                        # Decreasing column count by 1 since we do not want to show the index
+                        self.saved_table.setItem(row_count, col_count - 1, QtWidgets.QTableWidgetItem(parity))
+                    else:
+                        self.saved_table.setItem(row_count, col_count - 1, QtWidgets.QTableWidgetItem(str(data)))
 
 
 def call_error_msg_box(message):
