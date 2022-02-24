69a1222204ce7f92599118c1510fc2ecb895ac2d
Sourabh S Nath <sourabhsnath@gmail.com>
2022-02-24 08:55:44 +0530

    Working serial communication functionality

--- src/main_window.py
+++ src/main_window.py
@@ -1,15 +1,25 @@
 from PyQt6 import QtWidgets
 
-import serial_communication
 from gui import main_communication_window
+# import serial_communication
+from serial_communication import SerialCommunication
 
 
 class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow):
+    previous_read_data = ""
+
     def __init__(self):
         super().__init__()
+        self.serial_communication = SerialCommunication()
         self.setupUi(self)
+        self.serial_devices = []
+        self.current_device = {}
+        self.is_device_found = False
+        self.is_device_connected = False
         self.setup_baud_rate()
         self.search_device_button.clicked.connect(self.search_devices)
+        self.connect_button.clicked.connect(self.serial_connection)
+        self.send_message_button.clicked.connect(self.write_to_device)
 
     # Set Baud rates programmatically since the numbers aren't known.
     def setup_baud_rate(self):
@@ -18,9 +28,10 @@ class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow)
         for rate in baud_rates:
             self.baud_rate_combo_box.addItem(str(rate))
 
+    # Search all serial communication devices connected to the system
     def search_devices(self):
-        serial_devices = serial_communication.get_all_devices()
-        for i in serial_devices:
+        self.serial_devices = self.serial_communication.get_all_devices()
+        for i in self.serial_devices:
             for key in i:
                 if key == "Device":
                     self.device_combox_box.addItem(i[key])
@@ -28,8 +39,54 @@ class MainWindow(QtWidgets.QMainWindow, main_communication_window.Ui_MainWindow)
                     self.port_input.setText(i[key])
                 if key == "Serial Number":
                     self.serial_no_input.setText(i[key])
-                if key == "Port":
-                    serial_communication.port_location = (i[key])
+                # if key == "Port":
+                #     serial_communication.port_location = (i[key])
+
+        if len(self.serial_devices) != 0:
+            self.is_device_found = True
+
+    # Connect to the device currently seen in the combobox, currently connects to the first serial_device list item
+    # in the combo box.
+    # TODO: Get the id of the device when the user selects another device in the list through the combobox
+    def serial_connection(self):
+        if self.serial_devices:
+            if self.connect_button.text() == "Connect":
+                self.connect_button.setText("Disconnect")
+                device_name = self.device_combox_box.currentText()
+                if self.is_device_found:
+                    MainWindow.current_device = current_device = self.serial_devices[0]
+                    if current_device["Device"] == device_name:
+                        self.serial_communication.connection(current_device["Port"],
+                                                             self.baud_rate_combo_box.currentText())
+                        self.is_device_connected = self.serial_communication.get_connection_status()
+            else:
+                self.connect_button.setText("Connect")
+        else:
+            call_error_msg_box("Please select a device.")
+
+    def write_to_device(self):
+        if self.is_device_connected:
+            message = self.send_message_input.toPlainText()
+            print("Send to device", message)
+            self.serial_communication.write_data(message)
+            # This needs to be automatic by using threading or some observer pattern, here it just checks the
+            # previous value and makes changes if only the new value is different.
+            if self.serial_communication.received_data != MainWindow.previous_read_data:
+                MainWindow.previous_read_data = self.serial_communication.received_data
+                self.update_received_message(self.serial_communication.received_data)
+            self.send_message_input.clear()
+        else:
+            call_error_msg_box("Please check if the device is connected.")
+
+    def update_received_message(self, msg):
+        self.recieved_message_text_output.append(msg.decode("utf-8"))
+
+
+def call_error_msg_box(message):
+    msg = QtWidgets.QMessageBox()
+    msg.setText(message)
+    msg.setWindowTitle("Error")
+    msg.exec()
 
 
 if __name__ == "__main__":
--- src/serial_communication.py
+++ src/serial_communication.py
@@ -1,23 +1,44 @@
 import serial.tools.list_ports
 
-port_location = None
 
+class SerialCommunication:
+    received_data = ""
 
-def get_all_devices():
-    port_list = []
-    ports = serial.tools.list_ports.comports()
-    for p in sorted(ports):
-        # https://pyserial.readthedocs.io/en/latest/tools.html
-        # Don't know the difference between interface and product, since both are being reported as the same here.
-        port_list.append(
-            {"Port Name": p.name, "Port": p.device, "Serial Number": p.serial_number, "Interface": p.interface,
-             "Device": p.product})
-    print(port_list)
-    return port_list
+    def __init__(self):
+        self.port_list = []
+        self.serial_connection = None
+        self.is_connected = False
 
+    def get_all_devices(self):
+        ports = serial.tools.list_ports.comports()
+        for p in sorted(ports):
+            # https://pyserial.readthedocs.io/en/latest/tools.html
+            # Don't know the difference between interface and product, since both are being reported as the same here.
+            self.port_list.append(
+                {"Port Name": p.name, "Port": p.device, "Serial Number": p.serial_number, "Interface": p.interface,
+                 "Device": p.product})
+        print(self.port_list)
+        return self.port_list
 
-def send_message(msg, buadrate):
-    pass
+    def connection(self, port_location, baud_rate):
+        try:
+            self.serial_connection = serial.Serial(port_location, baudrate=baud_rate, timeout=1)
+            self.is_connected = self.serial_connection.is_open
+            print(self.serial_connection, type(self.serial_connection))
+        except Exception as e:
+            self.is_connected = False
+            print(f"Exception {e}")
 
+    def read_data(self):
+        if self.serial_connection is not None and self.is_connected:
+            SerialCommunication.received_data = self.serial_connection.readline()
+            print(SerialCommunication.received_data)
 
-get_all_devices()
+    def write_data(self, msg):
+        if self.serial_connection is not None and self.is_connected:
+            print("Inside write data", f"{self.serial_connection.port}", self.is_connected)
+            self.serial_connection.write(msg.encode())
+            self.read_data()
+
+    def get_connection_status(self):
+        return self.is_connected
